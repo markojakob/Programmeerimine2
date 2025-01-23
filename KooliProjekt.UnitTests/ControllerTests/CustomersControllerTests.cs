2020be6e347a8ddf457832da6c7a04b613ebd6be
markojakob <markjakobo@gmail.com>
2025-01-23 14:41:10 +0200

    Added Edit and Delete tests in ControllerTests

--- KooliProjekt.UnitTests/ControllerTests/CustomersControllerTests.cs
+++ KooliProjekt.UnitTests/ControllerTests/CustomersControllerTests.cs
@@ -52,5 +52,175 @@ namespace KooliProjekt.UnitTests.ControllerTests
             Assert.Equal(pagedResult, model.Data);
 
         }
+
+        [Fact]
+        public async Task Details_should_return_notfound_when_id_is_missing()
+        {
+            // Arrange
+            int? id = null;
+
+            // Act
+            var result = await _controller.Details(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Details_should_return_notfound_when_list_is_missing()
+        {
+            // Arrange
+            int id = 1;
+            var list = (Customer)null;
+            _customersServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Details(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Details_should_return_view_with_model_when_list_was_found()
+        {
+            // Arrange
+            int id = 1;
+            var list = new Customer { Id = id };
+            _customersServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Details(id) as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Details"
+            );
+            Assert.Equal(list, result.Model);
+        }
+
+        [Fact]
+        public void Create_should_return_view()
+        {
+            // Act
+            var result = _controller.Create() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Create"
+            );
+        }
+
+        [Fact]
+        public async Task Edit_should_return_notfound_when_id_is_missing()
+        {
+            // Arrange
+            int? id = null;
+
+            // Act
+            var result = await _controller.Edit(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Edit_should_return_notfound_when_list_is_missing()
+        {
+            // Arrange
+            int id = 1;
+            var list = (Customer)null;
+            _customersServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Edit(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Edit_should_return_view_with_model_when_list_was_found()
+        {
+            // Arrange
+            int id = 1;
+            var list = new Customer { Id = id };
+            _customersServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Edit(id) as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Edit"
+            );
+            Assert.Equal(list, result.Model);
+        }
+
+        [Fact]
+        public async Task Delete_should_return_notfound_when_id_is_missing()
+        {
+            // Arrange
+            int? id = null;
+
+            // Act
+            var result = await _controller.Delete(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Delete_should_return_notfound_when_list_is_missing()
+        {
+            // Arrange
+            int id = 1;
+            var list = (Customer)null;
+            _customersServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Delete(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Delete_should_return_view_with_model_when_list_was_found()
+        {
+            // Arrange
+            int id = 1;
+            var list = new Customer { Id = id };
+            _customersServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Delete(id) as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Delete"
+            );
+            Assert.Equal(list, result.Model);
+        }
     }
 }
--- KooliProjekt.UnitTests/ControllerTests/RentingsControllerTests.cs
+++ KooliProjekt.UnitTests/ControllerTests/RentingsControllerTests.cs
@@ -60,6 +60,185 @@ namespace KooliProjekt.UnitTests.ControllerTests
             Assert.Equal(pagedResult, model.Data);
         }
 
+        [Fact]
+        public async Task Details_should_return_notfound_when_id_is_missing()
+        {
+            // Arrange
+            int? id = null;
 
+            // Act
+            var result = await _controller.Details(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Details_should_return_notfound_when_list_is_missing()
+        {
+            // Arrange
+            int id = 1;
+            var list = (Renting)null;
+            _rentingServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Details(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Details_should_return_view_with_model_when_list_was_found()
+        {
+            // Arrange
+            int id = 1;
+            var list = new Renting { Id = id };
+            _rentingServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Details(id) as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Details"
+            );
+            Assert.Equal(list, result.Model);
+        }
+
+        [Fact]
+        public async void Create_should_return_view()
+        {
+            // Arrange
+
+            var customers = new List<Customer>();
+            _customerServiceMock
+                .Setup(x => x.Lookup())
+                .ReturnsAsync(customers);
+
+            // Act
+            var result = await _controller.Create() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Create"
+            );
+        }
+
+        [Fact]
+        public async Task Edit_should_return_notfound_when_id_is_missing()
+        {
+            // Arrange
+            int? id = null;
+
+            // Act
+            var result = await _controller.Edit(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Edit_should_return_notfound_when_list_is_missing()
+        {
+            // Arrange
+            int id = 1;
+            var list = (Renting)null;
+            _rentingServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Edit(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Edit_should_return_view_with_model_when_list_was_found()
+        {
+            // Arrange
+            int id = 1;
+            var list = new Renting { Id = id };
+            var customers = new List<Customer>();
+            _rentingServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+            _customerServiceMock
+                .Setup(x => x.Lookup())
+                .ReturnsAsync(customers);
+
+            // Act
+            var result = await _controller.Edit(id) as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Edit"
+            );
+            Assert.Equal(list, result.Model);
+        }
+
+        [Fact]
+        public async Task Delete_should_return_notfound_when_id_is_missing()
+        {
+            // Arrange
+            int? id = null;
+
+            // Act
+            var result = await _controller.Delete(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Delete_should_return_notfound_when_list_is_missing()
+        {
+            // Arrange
+            int id = 1;
+            var list = (Renting)null;
+            _rentingServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Delete(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Delete_should_return_view_with_model_when_list_was_found()
+        {
+            // Arrange
+            int id = 1;
+            var list = new Renting { Id = id };
+            _rentingServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Delete(id) as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Delete"
+            );
+            Assert.Equal(list, result.Model);
+        }
     }
 }
